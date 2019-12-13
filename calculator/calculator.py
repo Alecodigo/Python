c586909e8b512f59506cc700fe4f1b7c4e87adf6
Alejandro Sanchez <trabajosoftnet@outlook.es>
2019-12-12 21:43:59 -0500

    The program calculator.py was modified because we need to do better

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,15 +1,16 @@
-
+# -*- coding: utf-8 -*-
+#!/bin/python3
 
 
 class  Calculator:
-    
-    def __init__(self):
-        self.welcome_message = False
-        self.num = 0
-        self.operator = ''
-        self.result = 0
-        self.div_mult = 1
-        
+
+    def __init__(self, a):
+        self.display_hello()
+        self.num = a
+
+
+    def display_hello(self):
+
         self.IMAGES = ['''
          _____________________
         |  _________________  |
@@ -27,106 +28,46 @@ class  Calculator:
         |_____________________|
 
         ''',
-        # Result           
-        '''
-         _____________________
-        |  _________________  |
-        | |        {}       | |
-        | |_________________| |
-        |  ___ ___ ___   ___  |
-        | | 7 | 8 | 9 | | + | |
-        | |___|___|___| |___| |
-        | | 4 | 5 | 6 | | - | |
-        | |___|___|___| |___| |
-        | | 1 | 2 | 3 | | x | |
-        | |___|___|___| |___| |
-        | | . | 0 | = | | / | |
-        | |___|___|___| |___| |
-        |_____________________|
-
-        ''',
-
-        ]
-
-
-    def aritmetic(self, value, operator):
-        if operator == '-':
-            self.result -= value
-            return
-        if operator == '+':
-            self.result += value
-            return
-        if operator == '*':
-            self.div_mult *= value
-            self.result = self.div_mult
-            return
-        if operator == '/':
-            self.div_mult /= value
-            self.result = self.div_mult
-            return
-
-    def display(self):
-        if self.welcome_message == False:
-            print self.IMAGES[0]
-            self.welcome_message = True
-
-        while True:
-            print("\n\n")
-            self.num = raw_input("Please enter a numeric value")
-            self.operator = raw_input("Select +,-,/,* ")
-
-            if self.num.isdigit():
-                self.aritmetic(int(self.num), self.operator)
-                print self.IMAGES[1].format(self.result)
-            else:
-                print("Please enter just a numeric value")
+        ]   
+    
+    def rest(self):
         
-        
-            
+
+
+
+
+def actions():
+    calculator = Calculator()
+    
+    while True:
+        #Menu
+        command = input("""Select one actions in the menu
+                
+                [r]estar ----->  press r
+                [s]umar  ----->  press s
+                [d]ividir ---->  press d
+                [m]ultiplicar -> press m
+                [A]ny to exit
+
+                """)
+        if command == 'r':
+            calculator.rest()
+        elif command == 's':
+            calculator.suma()
+        elif command == 'd':
+            calculator.dividir()
+        elif command == 'm':
+            calculator.multi()
+        else:
+            break
+
+
 
 
 if __name__ == '__main__':
 
-    cal = Calculator()
 
     print("Hello, Welcome to Your Calculator Software")
-
-    cal.display()
-    
-
-    #Las lineas de abajo son necesarias si corremos el programa como ejecutable
-    print "\n\n"
-    var = raw_input('Press enter to exit')
+    calculator
 
 
-
-
-    # def aritmetic(self, value, operator):
-    #     if operator == '-':
-    #         self.result -= value
-    #         return
-    #     if operator == '+':
-    #         self.result += value
-    #         return
-    #     if operator == '*':
-    #         self.multi(value)
-    #         return
-    #     if operator == '/':
-    #         self.div(value)
-    #         return
-
-    # def display(self):
-    #     if self.welcome_message == False:
-    #         print self.IMAGES[0]
-    #         self.welcome_message = True
-
-    #     while True:
-    #         print("\n\n")
-    #         self.num = raw_input("Please enter a numeric value")
-    #         self.operator = raw_input("Select +,-,/,* ")
-
-    #         if self.num.isdigit():
-    #             self.aritmetic(int(self.num), self.operator)
-    #             print self.IMAGES[1].format(self.result)
-    #         else:
-    #             print("Please enter just a numeric value")
